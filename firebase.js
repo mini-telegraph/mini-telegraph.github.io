5cd32bcd7fd25c62662ffbb3c6f985cc98474e82
YegorAlexandrow <yegor99@live.com>
2019-07-12 20:54:06 +0300

    kek[A

--- firebase.js
+++ firebase.js
@@ -10,8 +10,48 @@ var firebaseConfig = {
 
 var user_name = "none";
 
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+function setCookie(name, value, options) {
+    options = options || {};
+  
+    var expires = options.expires;
+  
+    if (typeof expires == "number" && expires) {
+      var d = new Date();
+      d.setTime(d.getTime() + expires * 1000);
+      expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+      options.expires = expires.toUTCString();
+    }
+  
+    value = encodeURIComponent(value);
+  
+    var updatedCookie = name + "=" + value;
+  
+    for (var propName in options) {
+      updatedCookie += "; " + propName;
+      var propValue = options[propName];
+      if (propValue !== true) {
+        updatedCookie += "=" + propValue;
+      }
+    }
+  
+    document.cookie = updatedCookie;
+  }
+
 function updateUserName() {
-    user_name = "" + new Date().getTime()%100000*100 + Math.round(Math.random()*100);
+    user_name = getCookie("mt_name");
+    if(user_name == undefined) {
+        user_name = "" + new Date().getTime()*100 + Math.round(Math.random()*100);
+        setCookie("mt_name", user_name, {});
+    }
 
     alert(user_name);
 }
