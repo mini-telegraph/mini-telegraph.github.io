dbd0f7e84a956838ab09fbc96e6f4a927a8ffe08
YegorAlexandrow <yegor99@live.com>
2019-07-12 16:52:34 +0300

    initial

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCWLoH3IimIVAirC8Iw-0JR0VrD3bvH4bU",
+    authDomain: "minitelegraph.firebaseapp.com",
+    databaseURL: "https://minitelegraph.firebaseio.com",
+    projectId: "minitelegraph",
+    storageBucket: "minitelegraph.appspot.com",
+    messagingSenderId: "802906428032",
+    appId: "1:802906428032:web:20f3bd695f4709e6"
+};
+
+
+
+firebase.initializeApp(firebaseConfig);
