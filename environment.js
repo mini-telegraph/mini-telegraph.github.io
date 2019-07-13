db6c5a7af46459f47ac88bc703822901654b26e7
YegorAlexandrow <yegor99@live.com>
2019-07-13 16:19:59 +0300

    added restrictions

--- environment.js
+++ environment.js
@@ -1,15 +1,23 @@
 var last_time = 0;
 
+function scrollDown() {
+    var h = document.getElementById("history");
+    h.scrollTop = h.scrollHeight;
+}
+
 function addMessage(msg) {
     var d = new Date();
     document.getElementById("history").innerHTML +=
     '<div class="message_box"><span class="time">' + 
-    d.getHours() + ':' + d.getMinutes() +'</span>&#9993;&nbsp;' + 
+    (d.getHours() < 10 ? '0' : '') + d.getHours()  + ':' + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() +'</span>&#9993;&nbsp;' + 
     '<span class="message">' + msg + '</span></div>';
+
+    scrollDown();
 }
 
 function isSendAvailable() {
-    return document.getElementById("textbox").value.length > 0;
+    return (curr - last_time >= 30000) && 
+    document.getElementById("textbox").value.length > 0;
 }
 
 function clickSend() {
@@ -19,6 +27,25 @@ function clickSend() {
 
         addMessage(msg);
         sendMessage(msg);
+        last_time = new Date().getTime();
+        onType();
+    }
+}
+
+function onType() {
+    var l = document.getElementById("textbox").value.length
+    document.getElementById("text_limit").innerHTML = (l < 10 ? "&nbsp;" : "") + l + "/20";
+}
+
+function timeLoop() {
+    curr = new Date().getTime();
+    b = document.getElementById("button_send");
+    if(curr - last_time < 30000) {
+        b.disabled = true;
+        b.innerHTML = "" + parseInt(30 - (curr - last_time)/1000);
+    } else {
+        b.disabled = false;
+        b.innerHTML = "OK";
     }
 }
 
@@ -27,3 +54,6 @@ document.getElementById("textbox")
     event.preventDefault();
     if (event.keyCode === 13) clickSend();
 });
+
+scrollDown();
+setInterval(timeLoop, 500);
